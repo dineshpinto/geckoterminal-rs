0c28aa2eeb467015b063bc5cca48dc30921f5185
dineshpinto <annual.fallout_0z@icloud.com>
2024-03-16 17:36:30 +0530

    Add Debug macro to Network struct, update README.md

--- src/types/network.rs
+++ src/types/network.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Attributes {
     pub name: String,
     pub coingecko_asset_platform_id: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Network {
     pub id: String,
     #[serde(rename = "type")]
