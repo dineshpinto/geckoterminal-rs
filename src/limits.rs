447eee23092df939235d5c8c74099a788f4f2480
dineshpinto <annual.fallout_0z@icloud.com>
2024-03-16 19:42:32 +0530

    Remove unused limits

--- src/limits.rs
+++ src/limits.rs
@@ -1,11 +1,6 @@
 pub const MAX_PAGE: i32 = 10;
 pub const MAX_ADDRESSES: i32 = 30;
 
-pub const POOL_INCLUDES: [&str; 4] = ["base_token", "quote_token", "dex", "network"];
-pub const NETWORK_POOL_INCLUDES: [&str; 3] = ["base_token", "quote_token", "dex"];
-pub const TOKEN_INCLUDES: [&str; 1] = ["top_pools"];
-pub const TOKEN_INCLUDES_INFO: [&str; 1] = ["network"];
-
 pub const TIMEFRAMES: [&str; 3] = ["day", "hour", "minute"];
 pub const DAY_AGGREGATES: [i32; 1] = [1];
 pub const HOUR_AGGREGATES: [i32; 3] = [1, 4, 12];
